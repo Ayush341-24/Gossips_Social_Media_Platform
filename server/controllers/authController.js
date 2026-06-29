65d9a510f7eb74cf643e5369c4ae71089712f548
Ayush <ayushranjan1500@gmail.com>
2026-06-29 15:25:34 +0530

    Implement user registration with password hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,49 @@
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
+
+
 export const register = async (req , res) => {
     try{
-        res.status(200).json({
-            message : "Register route Working",
+        const {username , fullName , email , password} = req.body;
+        if(!username || !fullName || !email || !password){
+            return res.status(400).json({
+                message : "Please fill all the fields",
+            });
+        }
+
+        const existingUser = await User.findOne({
+            $or : [{email} , {username}]
+        });
+        if(existingUser){
+            return res.status(400).json({
+                message : "User already exists",
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password , salt);
+
+        const newUser = await User.create({
+            username , 
+            fullName ,
+            email ,
+            password : hashedPassword,
+        });
+
+        const userResponse = {
+            id : newUser._id,
+            username: newUser.username,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+            bio: newUser.bio,
+            followers: newUser.followers,
+            following: newUser.following,
+        }
+
+        res.status(201).json({
+            message : "User registered successfully",
+            user : userResponse,
         });
     }
     catch(error){
